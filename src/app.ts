6773eed16e4c046a10fc93d0e1a85fa09bb6312d
Higor Oliveira <higorosantos@gmail.com>
2025-05-07 23:27:23 -0300

    fix: corrigindo cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import  Express, { Application }  from "express";
 import httpError from "./middleware/http.error";
 import router from "./routes/coin.routes";
+import cors from "cors";
 
 
 const app:Application = Express();
+
+app.use(cors());
 app.use(Express.json());
 app.use('/api', router);
 app.use(httpError);
